e5bb4842330b79f3c9a367a67bc00b00a7150b09
Martin Brenn <brenn@depon.net>
2013-01-14 22:46:01 +0100

    Admin Root Structure

---
+++ src/BurnSystems.FlexBG/Modules/AdminInterfaceM/AdminRootData.cs
@@ -0,0 +1,45 @@
+﻿using BurnSystems.WebServer.Umbra.Views.Treeview;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSystems.FlexBG.Modules.AdminInterfaceM
+{
+    /// <summary>
+    /// Root object for data
+    /// </summary>
+    public class AdminRootData  : ITreeViewItem
+    {
+        public long Id
+        {
+            get { return 1; }
+        }
+
+        public string Title
+        {
+            get { return "FlexBG"; }
+        }
+
+        public string ImageUrl
+        {
+            get { return string.Empty; }
+        }
+
+        public bool IsExpandable
+        {
+            get { return true; }
+        }
+
+        public IEnumerable<ITreeViewItem> Children
+        {
+            get { return null; }
+        }
+
+        public override string ToString()
+        {
+            return "FlexBG Interface";
+        }
+    }
+}
