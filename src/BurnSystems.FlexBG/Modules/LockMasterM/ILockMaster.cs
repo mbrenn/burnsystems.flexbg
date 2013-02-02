97cc02f250efcdbb5fd947d86d6b334a02628304
Martin Brenn <brenn@depon.net>
2013-02-02 14:52:17 +0100

    First implementation, not implemented, of lockmaster

--- src/BurnSystems.FlexBG/Modules/LockMasterM/ILockMaster.cs
+++ src/BurnSystems.FlexBG/Modules/LockMasterM/ILockMaster.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace BurnSystems.FlexBG.Modules.LockMasterM
 {
+    /// <summary>
+    /// Defines the interface for the lock master
+    /// </summary>
     public interface ILockMaster
     {
+        /// <summary>
+        /// Acquires a read lock for the given entity type
+        /// </summary>
+        /// <param name="entityType">Entity type</param>
+        /// <param name="entityId">Id of the entity</param>
+        /// <returns>Disposable object that shall be disposed if finished</returns>
+        IDisposable AcquireReadLock(int entityType, long entityId);
+
+        /// <summary>
+        /// Acquires a write lock for the given entity type
+        /// </summary>
+        /// <param name="entityType">Entity type</param>
+        /// <param name="entityId">Id of the entity</param>
+        /// <returns>Disposable object that shall be disposed if finished</returns>
+        IDisposable AcquireWriteLock(int entityType, long entityId);
+
+        /// <summary>
+        /// Defines a relationship between owner and child
+        /// </summary>
+        /// <param name="ownerEntityType">Entity type of the owner</param>
+        /// <param name="childEntityType">Entity type of the child</param>
+        void AddRelationShip(int ownerEntityType, int childEntityType);
     }
 }
---
+++ src/BurnSystems.FlexBG/Modules/LockMasterM/Simple/SimpleLockMaster.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSystems.FlexBG.Modules.LockMasterM.Simple
+{
+    /// <summary>
+    /// A very simple lock master that just locks the complete game, but checks whether
+    /// we have the right order for entity types. 
+    /// </summary>
+    public class SimpleLockMaster : ILockMaster
+    {
+        public IDisposable AcquireReadLock(int entityType, long entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable AcquireWriteLock(int entityType, long entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRelationShip(int ownerEntityType, int childEntityType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
