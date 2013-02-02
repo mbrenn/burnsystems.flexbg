8324c50072196ca5b3787e34fe070022239ffb86
Martin Brenn <brenn@depon.net>
2013-02-02 19:45:48 +0100

    Entity type relationships

--- src/BurnSystems.FlexBG/Modules/DeponNet/EntityType.cs
+++ src/BurnSystems.FlexBG/Modules/DeponNet/EntityType.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BurnSystems.FlexBG.Modules.LockMasterM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -39,21 +40,43 @@ namespace BurnSystems.FlexBG.Modules.DeponNet
         /// <summary>
         /// Stores the entity type id of region
         /// </summary>
-        public const int Region = 6;
+        public const int Map = 6;
+
+        /// <summary>
+        /// Stores the entity type id of region
+        /// </summary>
+        public const int Region = 7;
 
         /// <summary>
         /// Stores the entity type id of research
         /// </summary>
-        public const int Research = 7;
+        public const int Research = 8;
 
         /// <summary>
         /// Stores the entity type id of unit groups
         /// </summary>
-        public const int UnitGroup = 8;
+        public const int UnitGroup = 9;
 
         /// <summary>
         /// Stores the entity type for server
         /// </summary>
         public const int Server = 100;
+
+        /// <summary>
+        /// Apply the relationships between the entity types to the lockmaster
+        /// </summary>
+        /// <param name="lockMaster">Lock master to be used</param>
+        public static void Apply(ILockMaster lockMaster)
+        {
+            lockMaster.AddRelationShip(Server, Game);
+            lockMaster.AddRelationShip(Game, Player);
+            lockMaster.AddRelationShip(Player, Building);
+            lockMaster.AddRelationShip(Player, Research);
+            lockMaster.AddRelationShip(Player, UnitGroup);
+            lockMaster.AddRelationShip(Player, Unit);
+            lockMaster.AddRelationShip(Player, Town);
+            lockMaster.AddRelationShip(Game, Region);
+            lockMaster.AddRelationShip(Game, Map);
+        }
     }
 }
