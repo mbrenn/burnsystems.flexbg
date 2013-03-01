7e8af9d4b4a9c0aaca1cf6b603d975bf51a51389
Martin Brenn <brenn@depon.net>
2013-03-01 20:51:51 +0100

    Backgroundworker is working

---
+++ src/BurnSystems.FlexBG/Modules/BackgroundWorkerM/Logic/ExecuteByRealTime.cs
@@ -0,0 +1,49 @@
+﻿using BurnSystems.FlexBG.Modules.BackgroundWorkerM.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSystems.FlexBG.Modules.BackgroundWorkerM.Logic
+{
+    /// <summary>
+    /// Returns a predicate, which executes an event by real time
+    /// </summary>
+    public class ExecuteByRealTime : ITimePredicate
+    {
+        /// <summary>
+        /// Stores the date of the last execution
+        /// </summary>
+        private DateTime lastExecution = DateTime.MinValue;
+
+        /// <summary>
+        /// Stores the intervaltimes
+        /// </summary>
+        private TimeSpan intervalTime;
+
+        public ExecuteByRealTime(TimeSpan intervalTime)
+        {
+            this.intervalTime = intervalTime;
+        }
+
+        /// <summary>
+        /// Gets the next execution time
+        /// </summary>
+        /// <param name="container">Container to be used</param>
+        /// <returns>Datetime of next</returns>
+        public DateTime GetNextExecutionTime(ObjectActivation.IActivates container)
+        {
+            return this.lastExecution + this.intervalTime;
+        }
+
+        /// <summary>
+        /// Refreshes the time
+        /// </summary>
+        /// <param name="container"></param>
+        public void RefreshTime(ObjectActivation.IActivates container)
+        {
+            this.lastExecution = DateTime.Now;
+        }
+    }
+}
---
+++ src/BurnSystems.FlexBG/Modules/BackgroundWorkerM/Logic/WorkerItem.cs
@@ -0,0 +1,70 @@
+﻿using BurnSystems.Collections;
+using BurnSystems.FlexBG.Modules.BackgroundWorkerM.Interface;
+using BurnSystems.ObjectActivation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSystems.FlexBG.Modules.BackgroundWorkerM.Logic
+{
+    /// <summary>
+    /// Stores the information about one worker
+    /// </summary>
+    class WorkerItem : IHasKey
+    {
+        /// <summary>
+        /// Initializes a new instance of the WorkerItem class. 
+        /// </summary>
+        public WorkerItem()
+        {
+            this.AssumedNextTime = DateTime.MinValue;
+        }
+
+        /// <summary>
+        /// Gets or sets the key
+        /// </summary>
+        public string Key
+        {
+            get;
+            set;
+        }
+        
+        /// <summary>
+        /// Gets or sets the assumed next time. 
+        /// </summary>
+        public DateTime AssumedNextTime
+        {
+            get;
+            set;
+        }
+
+        public ITimePredicate NextTime
+        {
+            get;
+            set;
+        }
+
+        public Action<IActivates> Action
+        {
+            get;
+            set;
+        }
+
+        public DateTime GetNextTime(IActivates container)
+        {
+            return this.NextTime.GetNextExecutionTime(container);
+        }
+
+        public void Execute(IActivates container)
+        {
+            this.Action(container);
+        }
+
+        internal void RefreshTime(IActivates container)
+        {
+            this.NextTime.RefreshTime(container);
+        }
+    }
+}
