edd620d1cd08b179d3969726faaab0d53580ed5e
Martin Brenn <brenn@depon.net>
2013-05-10 21:37:22 +0200

    Update

--- src/BurnSystems.FlexBG/Modules/DeponNet/ResearchM/Research.cs
+++ src/BurnSystems.FlexBG/Modules/DeponNet/ResearchM/Research.cs
@@ -6,30 +6,39 @@ using System.Threading.Tasks;
 
 namespace BurnSystems.FlexBG.Modules.DeponNet.ResearchM
 {
+    [Serializable]
     public class Research
     {
+        private long id;
+
+        private long ownerId;
+
+        private long researchTypeId;
+
+        private int level;
+
         public long Id
         {
-            get;
-            set;
+            get { return this.id; }
+            set { this.id = value; }
         }
 
         public long PlayerId
         {
-            get;
-            set;
+            get { return this.ownerId; }
+            set { this.ownerId = value; }
         }
 
         public long ResearchTypeId
         {
-            get;
-            set;
+            get { return this.researchTypeId; }
+            set { this.researchTypeId = value; }
         }
 
         public int Level
         {
-            get;
-            set;
+            get { return this.level; }
+            set { this.level = value; }
         }
     }
 }
